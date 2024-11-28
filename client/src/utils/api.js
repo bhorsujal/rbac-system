e49ecc2c0c251e5cdd63ae3bdad4973fbe2621ee
Sujal Bhor <bhorsujal@gmail.com>
2024-11-28 18:59:34 +0530

    url correction for api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8000/api/";
+const API_BASE = "https://xcept1on.pythonanywhere.com/api/";
 const axiosInstance = axios.create({
     baseURL: API_BASE,
     headers: {
